395f227e399bac103d79e2cd8e29dd3795914185
Hendrix Demers <hendrix.demers@mail.mcgill.ca>
2017-10-26 20:56:45 -0400

    Start the implementation of EELS raw spectrum data

---
+++ pysemeels/raw_spectrum.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+.. py:currentmodule:: pysemeels.raw_spectrum
+
+.. moduleauthor:: Hendrix Demers <hendrix.demers@mail.mcgill.ca>
+
+Raw EELS spectrum data.
+"""
+
+###############################################################################
+# Copyright 2017 Hendrix Demers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+# Standard library modules.
+
+# Third party modules.
+
+# Local modules.
+
+# Project modules.
+
+# Globals and constants variables.
+
+
+class RawSpectrum(object):
+    def __init__(self, name):
+        self.name = name
+
+    def read_hdf5(self, parent_group):
+        if self.name in parent_group:
+            project_group = parent_group[self.name]
+        else:
+            raise ValueError("The parent group does not contain the project")
+
+    def write_hdf5(self, parent_group):
+        project_group = parent_group.require_group(self.name)
---
+++ tests/test_raw_spectrum.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+.. py:currentmodule:: pysemeels.test_raw_spectrum
+
+.. moduleauthor:: Hendrix Demers <hendrix.demers@mail.mcgill.ca>
+
+Tests for the module :py:mod:`pysemeels.raw_spectrum`.
+"""
+
+###############################################################################
+# Copyright 2017 Hendrix Demers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+# Standard library modules.
+import unittest
+import os
+
+# Third party modules.
+import h5py
+
+# Local modules.
+
+# Project modules.
+from pysemeels import get_current_module_path
+from pysemeels.raw_spectrum import RawSpectrum
+
+
+# Globals and constants variables.
+
+
+class TestRawSpectrum(unittest.TestCase):
+    """
+    TestCase class for the module `pysemeels.raw_spectrum`.
+    """
+
+    def setUp(self):
+        """
+        Setup method.
+        """
+
+        unittest.TestCase.setUp(self)
+
+        self.name_ref = "TestRawSpectrum"
+        self.spectrum = RawSpectrum(self.name_ref)
+
+        self.test_data_path = get_current_module_path(__file__, '../test_data')
+
+    def tearDown(self):
+        """
+        Teardown method.
+        """
+
+        unittest.TestCase.tearDown(self)
+
+    def testSkeleton(self):
+        """
+        First test to check if the testcase is working with the testing framework.
+        """
+
+        # self.fail("Test if the testcase is working.")
+        self.assert_(True)
+
+    def test_init(self):
+        """
+        Test __init__ method.
+        """
+        name_ref = "TestRawSpectrum_init"
+        spectrum = RawSpectrum(name_ref)
+
+        self.assertEqual(name_ref, spectrum.name)
+
+        # self.fail("Test if the testcase is working.")
+
+    def test_write_hdf5(self):
+        """
+        Test write_hdf5 method.
+        """
+
+        filepath = os.path.join(self.test_data_path, "test_raw_spectrum_write_hdf5.hdf5")
+        with h5py.File(filepath, "w") as hdf5_file:
+
+            self.spectrum.write_hdf5(hdf5_file)
+
+            self.assertTrue(self.name_ref in hdf5_file)
+
+            root_group = hdf5_file[self.name_ref]
+
+        # self.fail("Test if the testcase is working.")
+
+        # os.remove(filepath)
+
+    def test_read_hdf5(self):
+        """
+        Test read_hdf5 method.
+        """
+
+        filepath = os.path.join(self.test_data_path, "test_raw_spectrum_read_hdf5.hdf5")
+        with h5py.File(filepath, "r") as hdf5_file:
+            self.spectrum.read_hdf5(hdf5_file)
+
+        # self.fail("Test if the testcase is working.")
+
+    def test_read_hdf5_bad_project(self):
+        """
+        Test read_hdf5 method with a different project name.
+        """
+
+        name_ref = "TestProject_init"
+        spectrum = RawSpectrum(name_ref)
+
+        filepath = os.path.join(self.test_data_path, "test_raw_spectrum_read_hdf5.hdf5")
+        with h5py.File(filepath, "r") as hdf5_file:
+            self.assertRaises(ValueError, spectrum.read_hdf5, hdf5_file)
+
+        # self.fail("Test if the testcase is working.")
+
+
+if __name__ == '__main__':  # pragma: no cover
+    import nose
+
+    nose.runmodule()
