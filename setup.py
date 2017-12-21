11fb650e31a39e642078a448fd663baf7b56850e
Hendrix Demers <hendrix.demers@mail.mcgill.ca>
2017-12-20 20:46:28 -0500

    Use a text file for requirements

--- setup.py
+++ setup.py
@@ -9,13 +9,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-with open('requirements.txt') as history_file:
-    requirements = history_file.read()
-
-# requirements = [
-#     "numpy",
-#     "h5py",
-# ]
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.read()
 
 test_requirements = [
     "nose",
